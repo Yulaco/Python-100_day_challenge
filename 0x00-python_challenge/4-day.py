55d7c739962aafab7047be09c5199bd535533084
Yulaco <cleochen93@gmail.com>
2023-10-27 12:21:14 -0700

    Day 4 code

---
+++ 0x00-python_challenge/4-day.py
@@ -0,0 +1,6 @@
+plant = input("Name a type of plant > ")
+cook = input("Name a method of cooking > ")
+food = input("Whatword describes burned food? > ")
+item = input("Name a DIY item > ")
+
+print(cook, foodName, "with ruined", plant, "on a bed of", item)
