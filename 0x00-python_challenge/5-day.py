021042d8eef6a3f7b8ca50baf710b424448103ee
Yulaco <cleochen93@gmail.com>
2023-10-28 05:37:42 -0700

    Day 5 code

---
+++ 0x00-python_challenge/5-day.py
@@ -0,0 +1,7 @@
+myName = input("What is your name?: ")
+
+if myName == "David":
+    print(f"Welcome Mr, {myName}")
+    print("You're one of the smartest person I've ever seen!")
+else:
+    print("What do you enjoy doing?: ")
