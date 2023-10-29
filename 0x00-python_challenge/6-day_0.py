f744c4e9ddff0ac4e39179123db4bcb9dbe3e371
Yulaco <cleochen93@gmail.com>
2023-10-29 11:41:19 -0700

    Day 6 code

---
+++ 0x00-python_challenge/6-day_0.py
@@ -0,0 +1,30 @@
+print("MY LOGIN SYSTEM")
+print()
+
+name = input("Username > ")
+password = input("Password > ")
+
+if (name == "David" and password == "dav0909"):
+    print(f"""Why hello there, {name}, what a lovely accent you have, you could have charmed your way in here even without a password.
+
+    Have a great day.
+
+    Don't forget to take a souvenir on your way out!""")
+
+elif (name == "Tom" and password == "Safe"):
+    print(f'''Why hello there, {name}, what a lovely accent you have, you could have charmed your way in here without a password.
+
+    Have a great day.
+
+    Don't forget to take a souvenir on your way out!''')
+
+elif (name "Jenny" and password == "cool43"):
+
+    print(f'''Why hello there, {name}, what a lovely accent you have, you could have charmed your way in here without a password.
+
+    Have a great day.
+
+    Don't forget to take a souvenir on your way out!''')
+
+else:
+    print("Oooops sorry wrong person!")
