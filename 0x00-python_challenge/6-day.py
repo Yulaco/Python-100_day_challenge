2ecbaec6ce76eec52d9b76976b648825fb36ca2d
Yulaco <cleochen93@gmail.com>
2023-10-29 11:40:49 -0700

    Day 6 code

---
+++ 0x00-python_challenge/6-day.py
@@ -0,0 +1,14 @@
+print("SECURE LOGIN")
+print()
+
+username = input("Username > ")
+password = input("Password > ")
+
+if username == "Mark" and password == "teamexcel":
+    print("Welcome Mark!")
+
+elif username == "Susan" and password == "suset7":
+    print("Welcome Susan!")
+
+else:
+    print("Wrong password, please try again!")
