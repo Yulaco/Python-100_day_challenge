3c0054ac829dcf170ce7188295d35f92cfc1cce6
Yulaco <cleochen93@gmail.com>
2023-10-30 11:16:52 -0700

    Day 7 code

---
+++ 0x00-python_challenge/7-day_0.py
@@ -0,0 +1,29 @@
+print("ORDER LIST")
+print()
+
+order = input("What would you like to order: pizza or hamburger?")
+
+if order == "hamburger":
+    print"Thank you.")
+    
+    chesse = input("Do you want cheese?")
+
+    if cheese == "yes":
+        print("You got it.")
+
+    else:
+        print("No cheese it is.")
+
+elif order == "pizza"
+    print("Pizza coming up.")
+    
+    toppings = input("Do you want pepperooni on that?")
+    
+    if toppings == "yes":
+        print("We will add pepperoni.")
+    
+    else:
+        print("Your pizza will not have pepperoni.")
+
+else:
+    print("Please reorder")
