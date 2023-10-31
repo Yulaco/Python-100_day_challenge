7e8c78939f378180cb035015ac2d5c5f8d76f86b
Yulaco <cleochen93@gmail.com>
2023-10-31 07:05:54 -0700

    Day 8 code

---
+++ 0x00-python_challenge/8-day_0.py
@@ -0,0 +1,22 @@
+print("AFFIRMATION GENERATOR OF THE DAY OF THE WEEK")
+print()
+print("==================================================")
+print()
+day = input("Please type a day of the week! ")
+print()
+if day == "Monday" or day == "monday":
+    print("New week, happy week!")
+elif day == "Tuesday" or day == "tuesday":
+    print("Keep working don't quit!")
+elif day == "Wednesday" or day == "wednesday":
+    print("Happy productive day!")
+elif day == "Thursday" or day == "thursday":
+    print("Keep going you are almost there!")
+elif day == "Friday" or day == "friday":
+    print("Yes, its friday you made it!")
+elif day == "Saturday" or day == "saturday":
+    print("Happy weekend!")
+elif day == "Sunday" or day == "sunday":
+    print("Keep on enjoying your weekend its almost over!")
+else:
+    print("Please type a correct day of the week!")
