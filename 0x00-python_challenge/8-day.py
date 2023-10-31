78d7668bcfb46369e8aaf6c42436910b17b417e7
Yulaco <cleochen93@gmail.com>
2023-10-31 07:05:32 -0700

    Day 8 code

---
+++ 0x00-python_challenge/8-day.py
@@ -0,0 +1,23 @@
+print("AFFIRMATIONS FOR THE WEEK")
+print("===========================================")
+
+name = input("What is your name? : ")
+day = input("Please input the day of the week: ")
+color = input("What is your favorite color? : ")
+
+if name == "Tom" or name == "tom":
+    print(f"Hello {name] is is great to have you here? : ")
+elif day == "Monday" or "white":
+    print(f"Happy {day}")
+    if color == "White" or "white":
+        print("You have good taste in color.")
+    elif color == "Yellow" or "yellow":
+        print(f"Wow {color}! That is a bright color.")
+    elif color == "Pink" or color == "pink":
+        print("You have such a sweet heart.")
+    elif color =="Black" or color "black":
+        print(f"{name}, that, {olor} is vry strong.")
+    else:
+        print("Please select a color.")
+else:
+    print("Thank you please try again")
