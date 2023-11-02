af8c34bef366355aa1f317203a37cbaff0cefd41
Yulaco <cleochen93@gmail.com>
2023-11-02 07:30:20 -0700

    Fix the code

---
+++ 0x00-python_challenge/9-day_1.py
@@ -0,0 +1,10 @@
+# Fix my code
+
+score = int(input("What is your score on the test? "))
+
+if score >= 80:
+    print("Not too bad")
+elif score > 70:
+    print("Acceptable.")
+else:
+    print("Please, you need to study more!")
