7bf0692c3184d24506597c3ce46c10437977a0f9
Yulaco <cleochen93@gmail.com>
2023-11-02 09:45:50 -0700

    Day 10 code

---
+++ 0x00-python_challenge/10-day.py
@@ -0,0 +1,22 @@
+# Program that calculates
+
+adding = 10 + 5
+print(adding)
+
+subtraction = 8 - 9
+print(subtraction)
+
+multiplication = 32 * 4
+print(multiplication)
+
+division = 50 / 5
+print(division)
+
+squared = 5 ** 2
+print(squared)
+
+modulo = 63 % 4
+print(modulo)
+
+divisor = 46 // 2
+print(divisor)
